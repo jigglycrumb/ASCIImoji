74a96d6e1054e2f61cb79365de9cfd23d0079c08
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2023-04-23 22:41:34 +0200

    fix: disable uglify

--- grunt/copy.js
+++ grunt/copy.js
@@ -5,29 +5,30 @@ module.exports = {
         expand: true,
         cwd: "src/chrome-extension",
         src: ["images/*", "js/*", "options/*", "manifest.json", "key.pem"],
-        dest: "dist/chrome-extension"
+        dest: "dist/chrome-extension",
       },
       // depends on jQuery build
       {
         expand: true,
         flatten: true,
         src: ["dist/js/asciimoji.js", "dist/jquery/asciimoji.jquery.js"],
-        dest: "dist/chrome-extension/js/"
-      }
-    ]
+        dest: "dist/chrome-extension/js/",
+      },
+    ],
   },
   homepage: {
     expand: true,
     cwd: "src/homepage",
     src: [
       // "googlea7755739bc343761.html",
+      "homepage.js",
       "index.html",
       "install.html",
       "images/icon-16.png",
       "images/header.png",
       "images/icloud-sync-*.png",
-      "images/macos-dictionary-installation-*.png"
+      "images/macos-dictionary-installation-*.png",
     ],
-    dest: "docs/"
+    dest: "docs/",
   },
 };
